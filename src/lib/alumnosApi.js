e464e7eee04240dd20e60cb8ff5bcd0978e7e63f
mmaidan <71332863+mmaidan@users.noreply.github.com>
2026-07-22 12:54:04 -0300

    Add files via upload

--- src/lib/alumnosApi.js
+++ src/lib/alumnosApi.js
@@ -188,6 +188,7 @@ export function interpretarCSV(texto, cursos) {
       gradoRaw,
       grado,
       divisionRaw,
+      divisionDetectada,
       cursoId,
       valido: Boolean(apellido && nombre && sexo && grado),
     };
